734dcf2527284c1d8d92dc79af995cbd13896e1b
xxx013126 <xxx013126@gmail.cjcu.edu.tw>
2017-06-01 15:09:24 +0800

    test2

--- app/src/main/java/com/example/cjcu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cjcu/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,16 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView list = (ListView) findViewById(R.id.list);
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
-        list.setAdapter(adapter);
+        //final ListView list = (ListView) findViewById(R.id.list);
+        final Spinner spinner=(Spinner) findViewById(R.id.spinner2);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+        //ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
+        ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this, R.array.fruit, R.layout.support_simple_spinner_dropdown_item);
+
+        //list.setAdapter(adapter);
+        spinner.setAdapter(adapter2);
+
+        /*list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this,list.getItemAtPosition(position).toString() , Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
     }
 }
